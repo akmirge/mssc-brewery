63af2ee3d585c21adb24bd2e47a55a6ef9f3ff71
Anil Mirge <manilche@gmail.com>
2020-12-03 20:36:00 -0500

    Updating the slf4j logging from debug to info

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getById(UUID id) {
-        log.debug("Get Customer by Id: " + id);
+        log.info("Get Customer by Id: " + id);
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("John Casey")
                 .build();
@@ -19,19 +19,19 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerDto save(CustomerDto customer) {
-       log.debug("Creating a new Customer");
+       log.info("Creating a new Customer");
        return CustomerDto.builder().id(UUID.randomUUID()).build();
     }
 
     @Override
     public void update(UUID id, CustomerDto customer) {
-        log.debug("Updating Customer with id: " + id);
+        log.info("Updating Customer with id: " + id);
         //TODO Implement Customer update
     }
 
     @Override
     public void delete(UUID id) {
-        log.debug("Deleting Customer with Id: " + id);
+        log.info("Deleting Customer with Id: " + id);
         //TODO Implement Customer delete
     }
 }
